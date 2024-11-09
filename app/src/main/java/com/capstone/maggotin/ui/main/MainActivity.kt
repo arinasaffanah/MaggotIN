791ba85c05b8076a6b5976baf84c484f233cda17
nuazsa <nurazissaputra@gmail.com>
2024-11-09 08:57:14 +0700

    v0.0.1

--- app/src/main/java/com/capstone/maggotin/ui/main/MainActivity.kt
+++ app/src/main/java/com/capstone/maggotin/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.capstone.maggotin
+package com.capstone.maggotin.ui.main
 
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -7,6 +7,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.capstone.maggotin.R
 import com.capstone.maggotin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
